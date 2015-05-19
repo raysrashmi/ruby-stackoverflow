e675ec0cf4bee29f740b68022a14516126351752
toshiyuki <toshiyuki.saito.16@gmail.com>
2015-05-19 14:40:45 +0900

    add fields

--- lib/ruby-stackoverflow/client/response_data.rb
+++ lib/ruby-stackoverflow/client/response_data.rb
@@ -1,14 +1,19 @@
 module RubyStackoverflow
   class Client
     class ResponseData
-      attr_reader :data, :has_more, :error
+      attr_reader :data, :has_more, :error, :raw_response, :raw_data, :quota_max, :quota_remaining, :backoff
 
       def initialize(response, klass)
+        @raw_response = response
         if response[:items].nil?
           @error = StackoverflowError.new(response)
         else
           @data = format_data(response[:items], klass)
           @has_more = response[:has_more]
+          @raw_data = response[:items]
+          @quota_max = response[:quota_max]
+          @quota_remaining = response[:quota_remaining]
+          @backoff = response[:backoff]
         end
       end
 
