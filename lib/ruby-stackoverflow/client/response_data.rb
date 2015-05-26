37666d638bda4ddbda07d4302e5cc5241e4bf84c
toshiyuki <toshiyuki.saito.16@gmail.com>
2015-05-26 17:09:48 +0900

    lazy load data

--- lib/ruby-stackoverflow/client/response_data.rb
+++ lib/ruby-stackoverflow/client/response_data.rb
@@ -1,20 +1,25 @@
 module RubyStackoverflow
   class Client
     class ResponseData
-      attr_reader :data, :has_more, :error, :raw_response, :raw_data, :quota_max, :quota_remaining, :backoff
+      attr_reader :has_more, :error, :raw_response, :raw_data, :quota_max, :quota_remaining, :backoff
 
       def initialize(response, klass)
         @raw_response = response
         if response[:items].nil?
           @error = StackoverflowError.new(response)
         else
-          @data = format_data(response[:items], klass)
-          @has_more = response[:has_more]
+          @klass = klass
           @raw_data = response[:items]
+          @has_more = response[:has_more]
           @quota_max = response[:quota_max]
           @quota_remaining = response[:quota_remaining]
           @backoff = response[:backoff]
         end
+      end
+
+      def data
+        return nil if @raw_data.nil?
+        @data ||= format_data(@raw_data.clone, @klass)
       end
 
       def format_data(data, klass)
